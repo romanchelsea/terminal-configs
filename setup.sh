36bf97e8b5240d331830c6211b0c64d0d2141b90
Roman Wang <romanwang888@gmail.com>
2019-04-19 22:09:55 -0700

    Let setup.sh configure git

--- setup.sh
+++ setup.sh
@@ -1,1 +1,21 @@
-echo "Setting up in home directory $HOME"
+set -e
+
+printf "Setting up home directory: $HOME\n\n"
+cd $HOME
+
+TERMINAL_CONFIG="$HOME/terminal-configs"
+if ! [[ -d $TERMINAL_CONFIG ]]; then
+
+    printf "Put terminal-config in the home directory!\n\n"
+    exit 1
+fi
+
+printf "Configuring Git\n\n"
+if [ -r .gitconfig ]; then
+    printf "A Git config file already exists.\n"
+    printf "Backing it up to ~/.gitconfig.backup\n"
+    printf "" > .gitconfig.backup
+    mv .gitconfig .gitconfig.backup
+fi
+
+ln -s $TERMINAL_CONFIG/.gitconfig .gitconfig
